8b16e44d37e2b598cbef9075bd9232f2f5cd35b2
Martin Allen <31280145+martyall@users.noreply.github.com>
2024-04-10 17:17:28 -0700

    Propagators (#4)
    * Added propagators dep
    
    * propogator solver

--- test/Test/Circuit/Arithmetic.hs
+++ test/Test/Circuit/Arithmetic.hs
@@ -1,4 +1,6 @@
 {-#LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Test.Circuit.Arithmetic where
 
 import           Circuit.Affine
@@ -7,12 +9,18 @@ import qualified Data.Map                     as Map
 import           Protolude
 import           Test.Tasty.QuickCheck
 import Data.Field.Galois (Prime)
+import Circuit.Solver
+import Data.Propagator (PropagatedNum, Propagated)
+import Data.List ((\\))
 
 -------------------------------------------------------------------------------
 -- Test values
 -------------------------------------------------------------------------------
 
 type Fr = Prime 21888242871839275222246405745257275088548364400416034343698204186575808495617
+
+instance KnownNat p => Propagated (Prime p)
+instance KnownNat p => PropagatedNum (Prime p)
 
 testEqualCircuit :: ArithCircuit Fr
 testEqualCircuit = ArithCircuit [Equal (InputWire 0) (IntermediateWire 0) (OutputWire 0)]
@@ -33,30 +41,28 @@ testSplitUnsplitCircuit nbits =
 -- Generators
 -------------------------------------------------------------------------------
 
-arbVars :: Int -> [Int] -> [Gen (AffineCircuit f Wire)]
-arbVars numInps mids =
-  varInps numInps ++ varMids mids
+arbVars :: [Int] -> [Int] -> [Gen (AffineCircuit f Wire)]
+arbVars inputs mids =
+  varInps inputs ++ varMids (mids \\ inputs)
   where
-    varInps size
-      | size <= 0 = []
-      | otherwise = [Var . InputWire <$> choose (0, numInps - 1)]
+    varInps _inputs = [Var . InputWire <$> elements _inputs]
     varMids [] = []
     varMids ms@(_ : _) = [Var . IntermediateWire <$> elements ms]
 
 arbAffineCircuitWithMids ::
   Arbitrary f =>
-  Int ->
+  [Int] ->
   [Int] ->
   Int ->
   Gen (AffineCircuit f Wire)
-arbAffineCircuitWithMids numInps mids size
+arbAffineCircuitWithMids inputs mids size
   | size <= 0 =
-    oneof $ [ConstGate <$> arbitrary] ++ arbVars numInps mids
+    oneof $ [ConstGate <$> arbitrary] ++ arbVars inputs mids
   | otherwise =
     oneof
-      [ ScalarMul <$> arbitrary <*> arbAffineCircuitWithMids numInps mids (size - 1),
-        Add <$> arbAffineCircuitWithMids numInps mids (size - 1)
-          <*> arbAffineCircuitWithMids numInps mids (size - 1)
+      [ ScalarMul <$> arbitrary <*> arbAffineCircuitWithMids inputs mids (size - 1),
+        Add <$> arbAffineCircuitWithMids inputs mids (size - 1)
+          <*> arbAffineCircuitWithMids inputs mids (size - 1)
       ]
 
 arbInputVector :: (Arbitrary f) => Int -> Gen (Map Int f)
@@ -67,16 +73,18 @@ arbArithCircuit ::
   -- | distribution of frequency of mul/equal/split
   -- gates, respectively
   (Int, Int, Int) ->
-  Int ->
+  [Int] ->
   Int ->
   Gen (ArithCircuit f)
-arbArithCircuit (distMul, distEqual, distSplit) numInps size
+arbArithCircuit (distMul, distEqual, distSplit) inputs size
   | size <= 0 =
     pure $ ArithCircuit []
   | otherwise =
     do
-      ArithCircuit gates <- arbArithCircuit (distMul, distEqual, distSplit) numInps (size - 1)
-      let mids = [i | IntermediateWire i <- concatMap outputWires gates]
+      ArithCircuit gates <- arbArithCircuit (distMul, distEqual, distSplit) inputs (size - 1)
+      let mids = 
+            [i | IntermediateWire i <- concatMap outputWires gates]
+
       frequency . catMaybes $
         [ (distMul,) <$> mulGate gates mids,
           (distEqual,) <$> equalGate gates mids,
@@ -85,11 +93,11 @@ arbArithCircuit (distMul, distEqual, distSplit) numInps size
   where
     mulGate gates mids =
       Just $ do
-        lhs <- arbAffineCircuitWithMids numInps mids 1
-        rhs <- arbAffineCircuitWithMids numInps mids 1
+        lhs <- arbAffineCircuitWithMids inputs mids 1
+        rhs <- arbAffineCircuitWithMids inputs mids 1
         let outWire = case mids of
-              [] -> 0
-              ms@(_ : _) -> maximum ms + 1
+              [] -> maximum inputs + 1
+              ms@(_ : _) -> maximum (ms <> inputs) + 1
             gate = Mul lhs rhs (IntermediateWire outWire)
         pure . ArithCircuit $ gates ++ [gate]
     equalGate _ [] =
@@ -97,7 +105,7 @@ arbArithCircuit (distMul, distEqual, distSplit) numInps size
     equalGate gates mids@(_ : _) =
       Just $ do
         inp <- elements mids
-        let outWire = maximum mids + 1
+        let outWire = maximum (mids <> inputs) + 1
             gate =
               Equal
                 (IntermediateWire inp)
@@ -109,7 +117,7 @@ arbArithCircuit (distMul, distEqual, distSplit) numInps size
     splitGate gates mids@(_ : _) =
       Just $ do
         inp <- IntermediateWire <$> elements mids
-        let firstOutWire = maximum mids + 1
+        let firstOutWire = maximum (mids <> inputs) + 1
             nbits = 256
             outWires = fmap IntermediateWire [firstOutWire .. firstOutWire + nbits - 1]
             gate = Split inp outWires
@@ -122,9 +130,9 @@ data ArithCircuitWithInputs f = ArithCircuitWithInputs (ArithCircuit f) [Map Int
 
 instance (Arbitrary f, Num f) => Arbitrary (ArithCircuitWithInputs f) where
   arbitrary = do
-    numVars <- abs <$> arbitrary `suchThat` (> 0)
-    program <- sized (arbArithCircuit (50, 10, 1) numVars)
-    inputs <- vectorOf 5 $ arbInputVector numVars
+    numInputs <- abs <$> arbitrary `suchThat` (> 0)
+    program <- sized (arbArithCircuit (50, 10, 1) [0 .. numInputs - 1])
+    inputs <- vectorOf 5 $ arbInputVector numInputs
     pure $ ArithCircuitWithInputs program inputs
 
 data ArithCircuitWithInput f = ArithCircuitWithInput (ArithCircuit f) (Map Int f)
@@ -132,9 +140,9 @@ data ArithCircuitWithInput f = ArithCircuitWithInput (ArithCircuit f) (Map Int f
 
 instance (Arbitrary f, Num f) => Arbitrary (ArithCircuitWithInput f) where
   arbitrary = do
-    numVars <- abs <$> arbitrary `suchThat` (> 0)
-    program <- sized (arbArithCircuit (50, 10, 1) numVars)
-    input <- arbInputVector numVars
+    numInputs <- abs <$> arbitrary `suchThat` (> 0)
+    program <- sized (arbArithCircuit (50, 10, 1) [0 .. numInputs - 1])
+    input <- arbInputVector numInputs
     pure $ ArithCircuitWithInput program input
 
 -------------------------------------------------------------------------------
@@ -144,3 +152,12 @@ instance (Arbitrary f, Num f) => Arbitrary (ArithCircuitWithInput f) where
 prop_arithCircuitValid :: ArithCircuitWithInputs Fr -> Bool
 prop_arithCircuitValid (ArithCircuitWithInputs program _) =
   validArithCircuit program
+
+prop_equivalentSolver :: ArithCircuitWithInput Fr -> Bool
+prop_equivalentSolver (ArithCircuitWithInput program inputs) =
+  solve inputs program  == 
+    evalArithCircuit 
+        (\w m -> Map.lookup (wireName w) m)
+        (\w m -> Map.insert (wireName w) m)
+        program
+        inputs
